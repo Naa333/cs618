fdeeab068fd7b5327156bb4c327b76110ad3e5e7
Naa333 <111006261+Naa333@users.noreply.github.com>
2025-09-04 21:00:37 +0000

    chore: displaying posts in the console from the database schema

--- src/example.js
+++ src/example.js
@@ -1,2 +1,12 @@
 import { initDatabase } from './db/init.js'
-initDatabase()
+import { Post } from './db/models/post.js'
+await initDatabase()
+const post = new Post({
+  title: 'Hello Mongoose!',
+  author: 'Naa Gyamfi',
+  contents: 'Connecting to the database using Mongoose',
+  tags: ['mongoose', 'mongodb'],
+})
+await post.save()
+const posts = await Post.find()
+console.log(posts)
