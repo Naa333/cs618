8fbd46e756e3573e411c5d8db1077b9340322caf
Naa333 <111006261+Naa333@users.noreply.github.com>
2025-10-17 23:30:07 +0000

    chore: server-side rendering and search engine optimization

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -6,6 +6,7 @@ import { PostFilter } from '../components/PostFilter.jsx'
 import { PostSorting } from '../components/PostSorting.jsx'
 import { getPosts } from '../api/posts.js'
 import { Header } from '../components/Header.jsx'
+import { Helmet } from 'react-helmet-async'
 
 export function Blog() {
   const [author, setAuthor] = useState('')
@@ -20,6 +21,13 @@ export function Blog() {
 
   return (
     <div style={{ padding: 8 }}>
+      <Helmet>
+        <title>Full-Stack React Blog</title>
+        <meta
+          name='description'
+          content='A blog full of articles about full-stackReact development.'
+        />
+      </Helmet>
       <Header />
       <br />
       <hr />
