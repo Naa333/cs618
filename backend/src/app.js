0b6b24b4c37d0f836a5ba068e7fe45bc74f0fa46
Naa333 <111006261+Naa333@users.noreply.github.com>
2025-10-22 18:47:43 +0000

    chore: including data aggregation and visualization

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import bodyParser from 'body-parser'
 import { postsRoutes } from './routes/posts.js'
 import { userRoutes } from './routes/users.js'
+import { eventRoutes } from './routes/events.js'
 
 const app = express()
 app.use(cors())
@@ -10,6 +11,7 @@ app.use(bodyParser.json())
 
 postsRoutes(app)
 userRoutes(app)
+eventRoutes(app)
 
 app.get('/', (req, res) => {
   res.send('Hello World from Express!')
